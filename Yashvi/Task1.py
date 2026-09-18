e066ea2218d5b62663bb7b0bf11d12d18c38af3b
Yashvi-k <21KAWAYa@hazelwick.org>
2026-09-18 12:20:44 +0000

    18/09/26

---
+++ Yashvi/Task1.py
@@ -0,0 +1,13 @@
+name=input("What is your name?")
+height=float(input("Enter your height:"))
+height_meters= (height/100)
+INCH= 2.54
+height_inches= (height/INCH)
+print("Hi"+name)
+print("Your height is:"+str(height_meters)+"m")
+print("Your height is:"+ str(height_inches)+"inch")
+if height > 180: 
+    print("True")
+else:
+    print("False")
+
